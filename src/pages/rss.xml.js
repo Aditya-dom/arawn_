8fc74492cb5a857e0ecfa6a01a5b0a79f10289be
Arawn <2300520400007@ietlucknow.ac.in>
2024-04-21 23:37:21 +0530

    fuck yeah! its working

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,22 @@
-import { defineCollection } from 'astro:content'
-import { rssSchema } from '@astrojs/rss'
+import rss from '@astrojs/rss'
 
-const blog = defineCollection({
-  schema: rssSchema,
-})
+import { formatBlogPosts } from '../js/utils'
 
-export const collections = { blog }
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true })
+const posts = formatBlogPosts(Object.values(postImportResult))
+
+export const get = () =>
+  rss({
+    title: "Arawn's Blog",
+    description: 'We live by the code & was raised by ethics.',
+    site: import.meta.env.SITE,
+    items: posts.map(post => ({
+      link: post.url,
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.date,
+      description: post.frontmatter.description,
+      customData: `
+      <author>${post.frontmatter.author}</author>
+    `,
+    })),
+  })
