7427090cc35acceedf03c575d24662f125c3399f
Arawn <2300520400007@ietlucknow.ac.in>
2024-06-26 13:56:17 +0530

    refactor: codecleanup

--- src/utils/setting-utils.ts
+++ src/utils/setting-utils.ts
@@ -1,3 +1,6 @@
+import {AUTO_MODE, DARK_MODE, DEFAULT_THEME, LIGHT_MODE} from "@constants/constants.ts";
+import type { LIGHT_DARK_MODE } from '@/types/config'
+
 export function getDefaultHue(): number {
   const fallback = '250'
   const configCarrier = document.getElementById('config-carrier')
@@ -16,4 +19,32 @@ export function setHue(hue: number): void {
     return
   }
   r.style.setProperty('--hue', hue)
+}
+
+
+export function applyThemeToDocument(theme: LIGHT_DARK_MODE) {
+  switch (theme) {
+    case LIGHT_MODE:
+      document.documentElement.classList.remove('dark')
+      break
+    case DARK_MODE:
+      document.documentElement.classList.add('dark')
+      break
+    case AUTO_MODE:
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
+      break
+  }
+}
+
+export function setTheme(theme: LIGHT_DARK_MODE): void {
+  localStorage.setItem('theme', theme)
+  applyThemeToDocument(theme)
+}
+
+export function getStoredTheme(): LIGHT_DARK_MODE {
+  return localStorage.getItem('theme') as LIGHT_DARK_MODE || DEFAULT_THEME
 }
