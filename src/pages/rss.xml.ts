cf835e234d34038dba8af9bbbac5910f9cb8f544
Arawn <2300520400007@ietlucknow.ac.in>
2024-08-28 23:50:03 +0530

    refactor: type safety

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -13,7 +13,7 @@ export async function GET(context: APIContext) {
   return rss({
     title: siteConfig.title,
     description: siteConfig.subtitle || 'No description',
-    site: context.site ?? 'https://fuwari.vercel.app',
+    site: context.site ?? 'https://arawn.live',
     items: blog.map(post => {
       const body = typeof post.data.body === 'string' ? post.data.body : ''
       return {
