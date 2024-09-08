940babd79257179e4d6c9f15d75e9915458f614e
Arawn <2300520400007@ietlucknow.ac.in>
2024-09-08 21:06:59 +0530

    oye lucky lucky oye

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,6 @@ import rehypeSlug from 'rehype-slug'
 import remarkDirective from 'remark-directive' /* Handle directives */
 import remarkGithubAdmonitionsToDirectives from 'remark-github-admonitions-to-directives'
 import remarkMath from 'remark-math'
-import svg from 'vite-plugin-svg'
 import { AdmonitionComponent } from './src/plugins/rehype-component-admonition.mjs'
 import { GithubCardComponent } from './src/plugins/rehype-component-github-card.mjs'
 import { parseDirectiveNode } from './src/plugins/remark-directive-rehype.js'
@@ -122,7 +121,6 @@ export default defineConfig({
     ],
   },
   vite: {
-    plugins: [svg()],
     build: {
       rollupOptions: {
         onwarn(warning, warn) {
